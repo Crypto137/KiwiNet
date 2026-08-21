85079ebf8f3640fbb12b8963fea71dc6665e378a
Crypto137 <Crypto137@users.noreply.github.com>
2026-08-21 13:52:30 +0300

    Break up PlayerComponent bytes into fields

--- src/KiwiNet.InstanceServer/GameObjects/Components/PlayerComponent.cs
+++ src/KiwiNet.InstanceServer/GameObjects/Components/PlayerComponent.cs
@@ -5,14 +5,31 @@ namespace KiwiNet.InstanceServer.GameObjects.Components
     public sealed class PlayerComponent : Component
     {
         public string Name { get; set; } = string.Empty;
+        public List<uint> PassiveSkills { get; } = new();
 
         public override void Serialize(Stream stream)
         {
             PacketIO.WriteString(stream, Name);
 
-            // dummy bytes
-            for (int i = 0; i < 30; i++)
-                PacketIO.WriteByte(stream, 0);
+            // 30 bytes
+
+            PacketIO.WriteByte(stream, 0);
+            PacketIO.WriteUInt32(stream, 0);
+
+            PacketIO.WriteInt32(stream, PassiveSkills.Count);
+            foreach (uint passiveSkill in PassiveSkills)
+                PacketIO.WriteUInt32(stream, passiveSkill);
+
+            PacketIO.WriteByte(stream, 0);
+            // WashedUp
+
+            PacketIO.WriteUInt32(stream, 0);
+
+            // one read of 16 bytes
+            PacketIO.WriteUInt32(stream, 0);
+            PacketIO.WriteUInt32(stream, 0);
+            PacketIO.WriteUInt32(stream, 0);
+            PacketIO.WriteUInt32(stream, 0);
         }
     }
 }
